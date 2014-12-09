7a4fdc0439bf1e4c631aeec586a9acb882e0b9b6
Christopher J. Volny <cvolny@gmail.com>
2014-12-09 00:04:31 -0500

    finished tutorial!

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,35 @@
-angular
-  .module('thinkster', []);
+(function () {
+    'use strict';
+
+    angular
+        .module('thinkster', [
+            'thinkster.config',
+            'thinkster.routes',
+            'thinkster.authentication',
+            'thinkster.layout',
+            'thinkster.posts',
+            'thinkster.utils',
+            'thinkster.profiles'
+        ]);
+
+    angular
+        .module('thinkster.config', []);
+
+    angular
+        .module('thinkster.routes', ['ngRoute']);
+
+    angular
+        .module('thinkster')
+        .run(run);
+
+    run.$inject = ['$http'];
+
+    /**
+     * @name run
+     * @desc Update xsrf $http headers to align with Django's defaults
+     */
+    function run($http) {
+        $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+        $http.defaults.xsrfCookieName = 'csrftoken';
+    }
+})();
