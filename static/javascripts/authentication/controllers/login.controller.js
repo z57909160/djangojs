aeef4a78f88e8e82e97fd7d54f92b253ecacbfb4
Mariano Ruiz <mrsarm@gmail.com>
2015-08-11 15:48:31 -0300

    Simplifying "login.controller.js" code

--- static/javascripts/authentication/controllers/login.controller.js
+++ static/javascripts/authentication/controllers/login.controller.js
@@ -7,39 +7,20 @@
 
     angular
         .module('thinkster.authentication.controllers')
-        .controller('LoginController', LoginController);
-
-    LoginController.$inject = ['$location', '$scope', 'Authentication'];
+        .controller('LoginController', ['$location', '$scope', 'Authentication', LoginController]);
 
     /**
      * @namespace LoginController
      */
     function LoginController($location, $scope, Authentication) {
-        var vm = this;
-
-        vm.login = login;
-
-        //activate();
-
-        /**
-         * @name activate
-         * @desc Actions to be performed when this controller is instantiated
-         * @memberOf thinkster.authentication.controllers.LoginController
-         */
-        function activate() {
-            // If the user is authenticated, they should not be here.
-            if (Authentication.isAuthenticated()) {
-                $location.url('/');
-            }
-        }
 
         /**
          * @name login
          * @desc Log the user in
          * @memberOf thinkster.authentication.controllers.LoginController
          */
-        function login() {
-            var p = Authentication.login(vm.email, vm.password);
+        this.login = function() {
+            var p = Authentication.login(this.email, this.password);
             p.then(function(value) {
                 if (value.status < 200 || value.status >299) {
                     $scope.error = value.data.message;
